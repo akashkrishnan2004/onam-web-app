7010ac53d112715cf862dedabbd429a02919496b
Akash Krishnan <akashkrishnan806@gmail.com>
2026-08-02 01:51:52 +0530

    Tenth commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -107,10 +107,11 @@ export default function Footer() {
 
           <p className="flex items-center gap-2 text-gray-400 md:text-xs">
             Made by{" "}
-            <span className="font-semibold text-yellow-300 md:text-xs">
+            {/* <span className="font-semibold text-yellow-300 md:text-xs">
               Akash Krishnan
-            </span>{" "}
-            with <FaHeart className="text-red-500 md:text-xs" /> for Kerala
+            </span>{" "} */}
+            {/* with  */}
+            <FaHeart className="text-red-500 md:text-xs" /> for Kerala
           </p>
 
           {/* <p className="flex items-center gap-2 text-gray-400">
